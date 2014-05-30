cd3b3e908c0d86b82687d078eb17f47358f8ac33
Matthew Printz <matt.printz@rackspace.com>
2014-05-30 14:53:30 -0500

    Switching grid representation to be more versitile.

--- test/ultimate_tic_tac_toe/core_test.clj
+++ test/ultimate_tic_tac_toe/core_test.clj
@@ -2,23 +2,38 @@
   (:require [clojure.test :refer :all]
             [ultimate-tic-tac-toe.core :refer :all]))
 
+(def test-grid [{:value nil :subgrid (load-subgrid ".........")} {:value :x  :subgrid (load-subgrid ".x..x..x.")} {:value nil :subgrid (load-subgrid ".........")}
+                {:value :o  :subgrid (load-subgrid "...ooo...")} {:value :x  :subgrid (load-subgrid "......xxx")} {:value nil :subgrid (load-subgrid ".........")}
+                {:value :o  :subgrid (load-subgrid "..o.o.o..")} {:value :x  :subgrid (load-subgrid "x...x...x")} {:value nil :subgrid (load-subgrid ".........")}])
+
 (deftest subgrid-winner
   (testing "Grids without three in a row have no winner"
     (is (nil? (winner empty-sub-grid)))
 
-    (is (nil? (winner [:x  :o  :x
-                       nil nil  :o
-                       :o  nil :x]))))
+    (is (nil? (winner (load-subgrid
+                      [:x  :o  :x
+                       nil nil :o
+                       :o  nil :x])))))
 
   (testing "Three in a row should win"
-    (is (= :x (winner [:x  :o  :x
+    (is (= :x (winner (load-subgrid
+                      [:x  :o  :x
                        nil :x  :o
-                       :o  nil :x])))
+                       :o  nil :x]))))
 
-    (is (= :x (winner [:x  :x  :x
+    (is (= :x (winner (load-subgrid
+                      [:x  :x  :x
                        nil nil :o
-                       :o  nil :x])))
+                       :o  nil :x]))))
 
-    (is (= :o (winner [:x nil :x
+    (is (= :o (winner (load-subgrid
+                      [:x nil :x
                        :o  :o :o
                        :x :o :x])))))
+
+  (testing "Testing empty super grid has no winner")
+    (is (nil? (winner empty-super-grid)))
+
+  (testing "Testing sub and super grids using same winner function"
+    (is (= :x (winner test-grid))))
+    (is (= :o (winner (:subgrid (nth test-grid 3))))))
